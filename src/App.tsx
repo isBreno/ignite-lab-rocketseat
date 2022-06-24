6e2fc29a3ced4f29384a7b2f95e4959bc882d4a5
Breno <eusoubrenovieira@gmail.com>
2022-06-23 22:59:25 -0300

    � | Aula 02 Concluida

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,9 @@
-import { gql, useQuery } from "@apollo/client";
-import { client } from "./lib/apollo";
-
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-    }
-  }
-`;
-
-interface Lesson {
-  id: string;
-  title: string;
-}
+import { Event } from "./pages/Event";
 
 function App() {
-  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY);
-  console.log(data);
-
   return (
     <div>
-      {data?.lessons.map((lesson: Lesson) => {
-        return <li key={lesson.id}>{lesson.title}</li>;
-      })}
+      <Event />
     </div>
   );
 }
