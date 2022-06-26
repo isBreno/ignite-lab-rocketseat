1da42b2dc17be32d6e08dca8cdfc9b073cbe28f8
Breno <eusoubrenovieira@gmail.com>
2022-06-26 14:53:52 -0300

    � | Aula 03 Concluida

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,20 @@
-import React from "react";
+import { Hamburger, List, X } from "phosphor-react";
+import React, { useContext, useState } from "react";
 import { Logo } from "./Logo";
 
 export default function Header() {
+  const [isActive, setisActive] = useState(false);
   return (
-    <header className="w-full py-5 flex items-center justify-center bg-gray-700 border-b border-gray-600">
+    <header className="w-full py-5 flex items-center justify-between px-4 lg:justify-center bg-gray-700 border-b border-gray-600">
       <Logo />
+      <div className="flex gap-2 items-center text-xl lg:hidden">
+        Aulas
+        {isActive ? (
+          <X size={36} onClick={() => setisActive(!isActive)} />
+        ) : (
+          <List size={36} onClick={() => setisActive(!isActive)} />
+        )}
+      </div>
     </header>
   );
 }
--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { CheckCircle, Lock } from "phosphor-react";
 import { isPast, format } from "date-fns";
 import ptBR from "date-fns/locale/pt-BR";
+import { Link } from "react-router-dom";
 
 interface LessonProps {
   title: string;
@@ -26,9 +27,9 @@ export default function Lesson({
   );
   return (
     <>
-      <a href="#">
+      <Link to={`/event/lesson/${slug}`} className="group">
         <span className="text-gray-300">{availableDateFormatted}</span>
-        <div className="rounded border border-gray-500 p-4 mt-2">
+        <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500 ">
           <header className="flex items-center justify-between">
             {isLessonAvailable ? (
               <span className="flex items-center gap-2 text-sm text-blue-500 font-medium">
@@ -48,7 +49,7 @@ export default function Lesson({
 
           <strong className="text-gray-200 mt-5 block">{title}</strong>
         </div>
-      </a>
+      </Link>
     </>
   );
 }
